3221dc2b0d4e712243e057088e4eb1103a023342
Mohamad Khatib <116002861+mohamad1999khatib@users.noreply.github.com>
2023-05-11 14:48:19 +0300

    ( SendMessage ) AND ( UART_Receive_DMA ) WORK Final

--- H1DR5/H1DR5.c
+++ H1DR5/H1DR5.c
@@ -370,6 +370,23 @@ void Module_Peripheral_Init(void){
 	MX_SPI1_Init();
 	lan_init();
 
+	//Circulating DMA Channels ON All Module
+	 for(int i=1;i<=NumOfPorts;i++)
+		{
+		  if(GetUart(i)==&huart1)
+				   { index_dma[i-1]=&(DMA1_Channel1->CNDTR); }
+		  else if(GetUart(i)==&huart2)
+				   { index_dma[i-1]=&(DMA1_Channel2->CNDTR); }
+		  else if(GetUart(i)==&huart3)
+				   { index_dma[i-1]=&(DMA1_Channel3->CNDTR); }
+		  else if(GetUart(i)==&huart4)
+				   { index_dma[i-1]=&(DMA1_Channel4->CNDTR); }
+		  else if(GetUart(i)==&huart5)
+				   { index_dma[i-1]=&(DMA1_Channel5->CNDTR); }
+		  else if(GetUart(i)==&huart6)
+				   { index_dma[i-1]=&(DMA1_Channel6->CNDTR); }
+		}
+
 	/* Create module special task (if needed) */
 	if(ProcessEthernetDataTaskHandle == NULL)
 		xTaskCreate(ProcessEthernetDataTask,(const char* ) "ProcessEthernetDataTask",configMINIMAL_STACK_SIZE,NULL,osPriorityNormal - osPriorityIdle,&ProcessEthernetDataTaskHandle);
