9674b17b21e576ddaf3c521cfbc41c683330d4c4
HALA-MUMMO\Hala MUMMO <halashem13@gmail.com>
2024-02-29 21:07:47 +0300

    - Edited release number in headers from V0.3.0 to V0.3.1

--- H1DR5/H1DR5_spi.c
+++ H1DR5/H1DR5_spi.c
@@ -1,21 +1,14 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    spi.c
-  * @brief   This file provides code for the configuration
+/*
+ BitzOS (BOS) V0.3.1 - Copyright (C) 2017-2024 Hexabitz
+ All rights reserved
+
+ File Name     : spi.c
+ Description   : This file provides code for the configuration
   *          of the SPI instances.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+
+ */
+
+
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "BOS.h"
--- H1DR5/H1DR5_spi.h
+++ H1DR5/H1DR5_spi.h
@@ -1,21 +1,12 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    spi.h
-  * @brief   This file contains all the function prototypes for
+/*
+ BitzOS (BOS) V0.3.1 - Copyright (C) 2017-2024 Hexabitz
+ All rights reserved
+
+ File Name     : spi.h
+ Description   : This file contains all the function prototypes for
   *          the spi.c file
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ */
+
 /* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __SPI_H__
