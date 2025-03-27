9a24dec5a6c1caa487c538c2285dedd259480f99
Mahmood_Reda <mr4141705@gmail.com>
2025-03-27 17:43:57 +0300

    add ((GetModuleParameter)) empty function

--- H1DR5/H1DR5.c
+++ H1DR5/H1DR5.c
@@ -39,7 +39,7 @@ extern uint8_t numOfRecordedSnippets;
 uint16_t length;
 
 
-module_param_t modParam[NUM_MODULE_PARAMS] ={{.paramPtr = NULL, .paramFormat =FMT_FLOAT, .paramName =""}};
+module_param_t modParam[NUM_MODULE_PARAMS] ={0};
 defaultValues defaultValue;
 /* Private variables ---------------------------------------------------------*/
 #define MX_SIZE_USER_BUFFER 512
@@ -455,6 +455,26 @@ void Module_Peripheral_Init(void){
 	 if(ProcessEthernetDataTaskHandle == NULL)
 	 xTaskCreate(ProcessEthernetDataTask,(const char* ) "ProcessEthernetDataTask",configMINIMAL_STACK_SIZE,NULL,osPriorityNormal - osPriorityIdle,&ProcessEthernetDataTaskHandle);
 
+}
+/***************************************************************************/
+/* This functions is useful only for input (sensors) modules.
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex,float *value){
+	Module_Status status =BOS_OK;
+
+	switch(paramIndex){
+
+		/* Invalid parameter index */
+		default:
+			status =BOS_ERR_WrongParam;
+			break;
+	}
+
+	return status;
 }
 
 /*-----------------------------------------------------------*/
