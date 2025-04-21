aff4b12139d13bd7776e61dfb2b9898eb1c5efa2
ADEL\adool <adoolfaki@gmail.com>
2025-04-21 12:03:33 +0300

    Refactory stage 1

--- H1DR5/H1DR5.h
+++ H1DR5/H1DR5.h
@@ -29,41 +29,33 @@
 
 #define	modulePN		_H1DR5
 
-
-/* Port-related definitions */
-#define	NumOfPorts			5
-
-#define P_PROG 				P2						/* ST factory bootloader UART */
+/* Port-related Definitions */
+#define	NUM_OF_PORTS	5
+#define P_PROG 			P2		/* ST factory bootloader UART */
 
 /* Define available ports */
-#define _P1 
-#define _P2 
-#define _P3 
-#define _P4 
-#define _P5 
-
+#define _P1
+#define _P2
+#define _P3
+#define _P4
+#define _P5
 
 /* Define available USARTs */
-
-#define _Usart2 1
-#define _Usart3 1
-#define _Usart4 1
-#define _Usart5 1
-#define _Usart6	1
-
+#define _USART2
+#define _USART3
+#define _USART4
+#define _USART5
+#define _USART6
 
 /* Port-UART mapping */
+#define UART_P1 &huart4
+#define UART_P2 &huart2
+#define UART_P3 &huart3
+#define UART_P4 &huart5
+#define UART_P5 &huart6
 
-#define P1uart &huart4
-#define P2uart &huart2
-#define P3uart &huart3
-#define P4uart &huart5
-#define P5uart &huart6
-
-
+/* Module-specific Hardware Definitions ************************************/
 /* Port Definitions */
-
-
 #define	USART2_TX_PIN		GPIO_PIN_2
 #define	USART2_RX_PIN		GPIO_PIN_3
 #define	USART2_TX_PORT		GPIOA
